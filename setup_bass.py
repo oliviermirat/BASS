eb0fdcf20729ffd0fdfd36de8707f753efb7e26b
olivierLenovo <miratoolivier@gmail.com>
2020-10-10 13:47:48 +0200

    very small change in original BASS code (for the compilation to work)

--- setup_bass.py
+++ setup_bass.py
@@ -2,5 +2,4 @@ from distutils.core import setup
 from Cython.Build import cythonize
 import numpy as np
 
-setup(name='BASS',
-      ext_modules=cythonize("bass.pyx",  compiler_directives={'language_level' : "3"},include_path=[np.get_include()]))
+setup(name='BASS', ext_modules=cythonize("bass.pyx",  compiler_directives={'language_level' : "3"},include_path=[np.get_include()]), include_dirs=[np.get_include()])
